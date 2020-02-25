73a64105ff7905f62205b103afa73f1370237169
keanh <keanh275@gmail.com>
2020-02-25 15:04:02 +0700

    add

--- SNT/src/Prime.java
+++ SNT/src/Prime.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Prime {
+    public static boolean isPrime(int number, int i){
+        if (number <= 2)
+            return (number == 2) ? true : false;
+        if (number % i == 0)
+            return false;
+        if (i * i > number)
+            return true;
+        return isPrime(number,i+1);
+    }
+
+    public static int findValue(int index) {
+        int count = 1;
+        int number = 2;
+        while (count <= index){
+            if (isPrime(number,2)){
+                if (index == count){
+                    return number;
+                }
+                count++;
+            }
+            number++;
+        }
+        return -1;
+    }
 }
--- SNT/src/PrimeTest.java
+++ SNT/src/PrimeTest.java
@@ -1,5 +1,15 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class PrimeTest {
 
+    @Test
+    void testIsPrime() {
+        int expected = 97;
+        Prime prime = new Prime();
+        int result = prime.findValue(25);
+        assertEquals(expected,result);
+    }
 }
