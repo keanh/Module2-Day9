70efa8d0b0324752a819e661a93d213b0d53e4bd
keanh <keanh275@gmail.com>
2020-02-25 11:33:57 +0700

    add

--- Triangle/src/TriangleClassifier.java
+++ Triangle/src/TriangleClassifier.java
@@ -1,12 +1,14 @@
 public class TriangleClassifier {
 
     public static String checkTriangle(int edge1, int edge2, int edge3) {
-        if (edge1 == edge2 && edge1 == edge3) {
-            return "Tam giác đều";
-        } else if (edge1 == edge2 || edge1 == edge3 || edge2 == edge3 && edge1 > 0 && edge2 > 0 && edge3 > 0){
-            return "Tam giác cân";
-        }else if ((edge1 + edge2 > edge3) && (edge1 + edge3 > edge2) && (edge2 + edge3 > edge1)){
-            return "Tam giác thường";
+        if ((edge1 + edge2 > edge3) && (edge1 + edge3 > edge2) && (edge2 + edge3 > edge1)){
+            if (edge1 == edge2 && edge1 == edge3) {
+                return "Tam giác đều";
+            } else if (edge1 == edge2 || edge1 == edge3 || edge2 == edge3 && edge1 > 0 && edge2 > 0 && edge3 > 0){
+                return "Tam giác cân";
+            } else {
+                return "Tam giác thường";
+            }
         }else {
             return "Đây không phải tam giác";
         }
